bc545b81f20430140ece131caae489728f8bec92
Paulina Zhang <zhan7856@umn.edu>
2024-10-28 23:08:46 -0500

    kfalksdjf

--- project_iteration_1-team11/app/src/main/java/edu/umn/cs/csci3081w/project/model/Bus.java
+++ project_iteration_1-team11/app/src/main/java/edu/umn/cs/csci3081w/project/model/Bus.java
@@ -184,4 +184,8 @@ public class Bus extends Vehicle {
     return nextStop;
   }
 
+  public int calculateCO2() {
+    return (2 * this.getPassengers().size()) + 4;
+  }
+
 }
--- project_iteration_1-team11/app/src/main/java/edu/umn/cs/csci3081w/project/model/Train.java
+++ project_iteration_1-team11/app/src/main/java/edu/umn/cs/csci3081w/project/model/Train.java
@@ -184,4 +184,7 @@ public class Train extends Vehicle {
     return nextStop;
   }
 
+  public int calculateCO2() {
+    return (3 * this.getPassengers().size()) + 6;
+  }
 }
